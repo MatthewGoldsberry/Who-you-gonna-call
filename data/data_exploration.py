975314e789f93d102482eabf129a931f91d9c0fa
Matthew Goldsberry <mattheweg99@gmail.com>
2026-03-12 00:52:47 -0400

    documentation of some data exploration done on the csv

---
+++ data/data_exploration.py
@@ -0,0 +1,30 @@
+"""Basic Python script to find out some information about a couple of the columns that are getting colored."""
+
+import csv
+from pathlib import Path
+
+path = Path(__file__).parent / "Cincinnati_311_(Non-Emergency)_Service_Requests_20260227.csv"
+
+priorities = set()
+departments = set()
+neighborhoods = set()
+
+with path.open(encoding="utf8") as f:
+    reader = csv.DictReader(f)
+    
+    for row in reader:
+        priorities.add(row.get("PRIORITY", "").strip())
+        departments.add(row.get('DEPT_NAME', "").strip())
+        neighborhoods.add(row.get("NEIGHBORHOOD", "").strip())
+
+print("=== UNIQUE PRIORITIES ===")
+print(f"Length: {len(list(filter(None, priorities)))}")
+print(sorted(list(filter(None, priorities))))
+
+print("\n=== UNIQUE DEPARTMENTS ===")
+print(f"Length: {len(list(filter(None, departments)))}")
+print(sorted(list(filter(None, departments))))
+
+print("\n=== UNIQUE NEIGHBORHOODS ===")
+print(f"Length: {len(list(filter(None, neighborhoods)))}")
+print(sorted(list(filter(None, neighborhoods))))
