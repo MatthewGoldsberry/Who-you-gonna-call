c26e85ca2c2815080290e003e9b3c150f757bd0a
Matthew Goldsberry <mattheweg99@gmail.com>
2026-03-11 20:59:23 -0400

    filter data to pothole to start

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
 d3.csv('data/Cincinnati_311_(Non-Emergency)_Service_Requests_20260227.csv')
 .then(data => {
-    console.log("number of items: " + data.length);
+    // TODO this is the temporary filter mentioned in level 1 description -- will need removed later
+    const filteredData = data.filter(d => d.SR_TYPE === 'PTHOLE');
 
-    data.forEach(d => {  //convert from string to number
+    filteredData.forEach(d => {  //convert from string to number
       d.latitude = +d.LATITUDE; 
       d.longitude = +d.LONGITUDE;  
     });
 
     // Initialize chart and then show it
-    leafletMap = new LeafletMap({ parentElement: '#my-map'}, data);
+    leafletMap = new LeafletMap({ parentElement: '#my-map'}, filteredData);
 
 
   })
