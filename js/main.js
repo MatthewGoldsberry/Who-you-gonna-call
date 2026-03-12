d63085acb994382792d42a2c4bb5a41cfa8fd17d
Matthew Goldsberry <mattheweg99@gmail.com>
2026-03-11 23:55:39 -0400

    calculate elements missing coords and update in vis

--- js/main.js
+++ js/main.js
@@ -3,13 +3,24 @@ d3.csv('data/Cincinnati_311_(Non-Emergency)_Service_Requests_20260227.csv')
     // TODO this is the temporary filter mentioned in level 1 description -- will need removed later
     const filteredData = data.filter(d => d.SR_TYPE === 'PTHOLE');
 
-    filteredData.forEach(d => {  //convert from string to number
-      d.latitude = +d.LATITUDE; 
-      d.longitude = +d.LONGITUDE;  
+    // filter data down to separate rows with coords out for the leaflet map
+    let missingCoordsCount = 0;
+    let validData = [];
+    filteredData.forEach(d => {
+        if (!d.LATITUDE || !d.LONGITUDE || d.LATITUDE.trim() === "" || d.LONGITUDE.trim() === "") {
+            missingCoordsCount++;
+        } else {
+            d.latitude = +d.LATITUDE; 
+            d.longitude = +d.LONGITUDE;
+            validData.push(d);
+        }  
     });
 
-    // Initialize chart and then show it
-    leafletMap = new LeafletMap({ parentElement: '#my-map'}, filteredData);
+    // visually log how many elements were not mapped because of missing coordinates
+    d3.select('#unmapped-count').text(missingCoordsCount);
+
+    // initialize chart and then show it
+    leafletMap = new LeafletMap({ parentElement: '#my-map'}, validData);
 
 
   })
