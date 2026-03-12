29e8e9aa3db58a85df5117a1f8b1878ae175a078
Matthew Goldsberry <mattheweg99@gmail.com>
2026-03-12 17:31:35 -0400

    set back to filtering to just potholes for testing until we get further (goal 6)

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ let leafletMap;
 d3.csv('data/Cincinnati_311_(Non-Emergency)_Service_Requests_20260227.csv')
 .then(data => {
     // TODO this is the temporary filter mentioned in level 1 description -- will need removed later good for testing
-    // data = data.filter(d => d.SR_TYPE === 'PTHOLE');
+    data = data.filter(d => d.SR_TYPE === 'PTHOLE');
 
     // filter data down to separate rows with coords out for the leaflet map
     let missingCoordsCount = 0;
